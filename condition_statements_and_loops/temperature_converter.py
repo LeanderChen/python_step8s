52d105519766c6b90b45f84b5867df1fefd60f41
leander <2310551143@qq.com>
2019-04-20 11:40:01 +0800

    init project structure

---
+++ condition_statements_and_loops/temperature_converter.py
@@ -0,0 +1,14 @@
+temp = input('请输入摄氏或华氏温度数值（形如20c、35c、150f...）：')
+type = temp[-1].upper()
+value = int(temp[:-1])
+
+if type == 'C':
+  res = int(round(value/5*9 + 32))
+elif type == 'F':
+  res = int(round((value -32) / 9 * 5))
+else:
+  print('请输入有效温度格式')
+  quit()
+
+print('转换结果为：', res, ('华氏度' if type == 'C' else '摄氏度'))
+quit()
